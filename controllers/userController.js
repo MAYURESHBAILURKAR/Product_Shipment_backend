24999965675153831348c85a8e6376f1195efd5c
Mayuresh Bailurkar <mayureshbailurkar@gmail.com>
2026-09-03 18:31:03 +0530

    locality change

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,14 +16,14 @@ const getUsers = async (req, res) => {
 // @desc    Create a new production user
 // @route   POST /api/users
 const createUser = async (req, res) => {
-  const { name, email, password, mobile, priceAllotted } = req.body;
-  
+  const { name, email, password, mobile, locality, priceAllotted } = req.body;
+
   try {
     const userExists = await User.findOne({ email });
     if (userExists) return res.status(400).json({ message: 'User already exists' });
 
     const user = await User.create({
-      name, email, password, mobile, priceAllotted, role: 'production'
+      name, email, password, mobile, locality, priceAllotted, role: 'production'
     });
 
     res.status(201).json(user);
